bff3aa7c3c3190c26e4db67e88dd5f7748966fb4
Dmitriy Krupko <dimonkrupko@yandex.ru>
2022-03-14 17:38:08 +0300

    initial

---
+++ src/main.java
@@ -0,0 +1,43 @@
+
+жpublic class main {
+    public static void main(String[] args) {
+        int coffeeAmount = 1;
+        int milkAmount = 1;
+        int skimmedMilkAmount = 2;
+        int noLactoseMilkAmount = 2;
+
+        boolean isBlocked = false;
+
+        int capuccinoMilkRequired = 60;
+        int capuccinoCoffeeRequired = 15;
+
+        boolean milkIsEnough = milkAmount >= capuccinoMilkRequired ||
+                skimmedMilkAmount >= capuccinoMilkRequired ||
+                noLactoseMilkAmount >= capuccinoMilkRequired;
+
+        boolean coffeeIsEnough = coffeeAmount >= capuccinoCoffeeRequired;
+
+        boolean hasErrors = false;
+
+        if (isBlocked) {
+            System.out.println("Кофе-машина заблокированна");
+            hasErrors = true;
+        }
+
+        if (!coffeeIsEnough) {
+            System.out.println("Кофе недостаточно");
+            hasErrors = true;
+        }
+
+        if (!milkIsEnough) {
+            System.out.println("Молока недостаточно");
+            hasErrors = true;
+        }
+
+        if (!hasErrors) {
+            System.out.println("Готовим кофе");
+
+        }
+
+    }
+}
